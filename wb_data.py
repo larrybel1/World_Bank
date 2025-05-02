9aa376ce5c0231df86feaefe572bae89e0bcbab1
Larry Bello-Castillo <belarry2001@gmail.com>
2025-05-02 00:33:00 -0400

    First Push

---
+++ wb_data.py
@@ -0,0 +1,64 @@
+import plotly.express as px
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime
+import wbgapi as wb
+import difflib
+
+def get_country_code(country_name):
+    country_name = country_name.strip().lower()
+    countries = {c['value'].lower(): c['id'] for c in wb.economy.list()}
+    
+    if country_name in countries:
+        return countries[country_name]
+    # Use closest match if no exact match
+    closest = difflib.get_close_matches(country_name, countries.keys(), n=1)
+    if closest:
+        return countries[closest[0]]
+    else:
+        return None
+
+
+# Ask the user for a keyword to search for indicators
+search_keyword = str(input('What keyword are you interested in looking for in indicators?: '))
+
+# List all available indicators
+indicators_list = []
+df = []
+
+# Initialize counter
+choice = 1
+# Loop and print indicators matching the keyword
+for indicator in wb.series.list():
+    id_title = indicator['id']
+    indicator_name = indicator['value']  # ← Fix is here!
+
+    if search_keyword.lower() in indicator_name.lower():
+        print(f"{choice}: {id_title} - {indicator_name}")
+        choice += 1
+        indicators_list.append(indicator)
+selection = int(input('\nEnter the number of the indicator you want to use: ')) - 1
+chosen_indicator = indicators_list[selection]['id']
+# print(chosen_indicator)
+country_pick = input('Pick a country you would like to take a look at: ')
+country_code = get_country_code(country_pick)
+# print(country_code)
+
+if not country_code:
+    print('Country could not be found, please try again with different spelling: ')
+else:
+    df = wb.data.DataFrame(chosen_indicator, economy=country_code)#, time=range(2000, 2024))
+# print(df)
+flipped_df = df.transpose().reset_index()
+# print(flipped_df)
+flipped_df.columns = ['Year', 'GDP']
+# print(flipped_df)
+fig = px.line(
+    flipped_df,
+    x ='Year',                  # x-axis: years
+    y = flipped_df.columns[1],           # y-axis: the indicator (3rd column = renamed indicator name)
+    title = f"{flipped_df.columns[1]} in {country_pick.title()} over the {flipped_df.columns[0]}s",
+    markers = True
+)
+
+fig.show()
