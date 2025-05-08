057faa9cf53dc1ecb76820a287abf805a4cd82aa
Larry Bello-Castillo <belarry2001@gmail.com>
2025-05-08 00:33:32 -0400

    played around with file 3 to look at trends in all of the countries

--- wb_data_3.py
+++ wb_data_3.py
@@ -1,76 +1,54 @@
 import plotly.express as px
 import wbgapi as wb
 import difflib
-
-# Function made to look for the country code
-def get_country_code(country_name):
-    country_name = country_name.strip().lower()
-    countries = {c['value'].lower(): c['id'] for c in wb.economy.list()}
-    
-    if country_name in countries:
-        return countries[country_name]
-    # Use closest match if no exact match
-    closest = difflib.get_close_matches(country_name, countries.keys(), n=1)
-    if closest:
-        return countries[closest[0]]
-    else:
-        return None
-
-
+import pandas as pd
 # Ask the user for a keyword to search for indicators
 search_keyword = str(input('What keyword are you interested in looking for in indicators?: '))
+search_keyword_2 = str(input('What keyword are you interested in looking for in the second indicators?: '))
 
 # List all available indicators
 indicators_list = []
+indicators_list_2 = []
 df = []
+df_2 = []
+chosen_indicator = ' '
+chosen_indicator_2 = ' '
 
-# Initialize counter
-choice = 1
-# Loop and print indicators matching the keyword
-for indicator in wb.series.list():
-    id_title = indicator['id']
-    indicator_name = indicator['value']  # ← Fix is here!
+def indicators(x,y):
+    global chosen_indicator
+    global chosen_indicator_2
+    choice = 1
+    choice_2 = 1
+    # Loop and print indicators matching the keyword
+    for x in wb.series.list():
+        id_title = x['id']
+        indicator_name = x['value']
 
-    if search_keyword.lower() in indicator_name.lower():
-        print(f"{choice}: {id_title} - {indicator_name}")
-        choice += 1
-        indicators_list.append(indicator)
-selection = int(input('\nEnter the number of the indicator you want to use: ')) - 1
-chosen_indicator = indicators_list[selection]['id']
-# print(chosen_indicator)
+        if search_keyword.lower() in indicator_name.lower():
+            print(f"{choice}: {id_title} - {indicator_name}")
+            choice += 1
+            indicators_list.append(x)
+    selection = int(input('\nEnter the number of the indicator you want to use: ')) - 1
+    chosen_indicator = indicators_list[selection]['id']
 
-# Allows the user to pick two different countries 
-country_pick = input('Pick a country you would like to take a look at: ')
-country_pick2 = input('Pick a second country you would like to take a look at: ')
-country_code = get_country_code(country_pick)
-country_code2 = get_country_code(country_pick2)
-# print(country_code)
+    # Loop and print second indicators matching the keyword
+    for y in wb.series.list():
+        id_title_2 = y['id']
+        indicator_name_2 = y['value'] 
 
-# This piece looks for the country to be spelt correctly 
-if not country_code:
-    print('Country could not be found, please try again with different spelling: ')
-else:
-    df = wb.data.DataFrame(chosen_indicator, economy= [country_code, country_code2])#.reset_index()#, time=range(2000, 2024))
-# print(df)
+        if search_keyword_2.lower() in indicator_name_2.lower():
+            print(f"{choice_2}: {id_title_2} - {indicator_name_2}")
+            choice_2 += 1
+            indicators_list_2.append(y)
+    selection_2 = int(input('\nEnter the number of the second indicator you want to use: ')) - 1
+    chosen_indicator_2 = indicators_list_2[selection_2]['id']
 
-# This sets up the df to be easier for the px.line() to read it easier 
-flipped_df = df.transpose().reset_index()#.melt()
-flipped_df.columns = ['Years', country_pick, country_pick2]
-# print(flipped_df)
 
-# Creates the line graph comparies boths countries and the single indicator chosen
-fig = px.line(
-    flipped_df,
-    x = 'Years',                  # x-axis: years
-    y = [country_pick, country_pick2],           # y-axis: the indicator (3rd column = renamed indicator name)
-    title = f"{search_keyword} in {country_pick.title()} and {country_pick2.title()} over the {flipped_df.columns[0]}s",
-    markers = True,
-    color_discrete_map = {country_pick: 'blue', country_pick2: 'green'},
-     labels={
-        'Year': 'Time (Years)',      # Custom x-axis label
-        'value': f'{search_keyword}',       # y-axis label if you're using long-form data
-        'variable': 'Countries'        # shown in legend for wide-form data
-    }
-)
+indicators(search_keyword,search_keyword_2)
 
-fig.show()
+df = wb.data.DataFrame(chosen_indicator)
+df_2 = wb.data.DataFrame(chosen_indicator_2)
+
+print(df.head())
+print(df_2.head())
+
