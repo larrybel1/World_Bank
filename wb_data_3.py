db50a34b3c683e2fbd15c12913d35e558df4163a
Larry Bello-Castillo <belarry2001@gmail.com>
2025-05-08 08:49:56 -0400

    more changes, trying to merge both dfs but so far unsuccessfull

--- wb_data_3.py
+++ wb_data_3.py
@@ -2,23 +2,25 @@ import plotly.express as px
 import wbgapi as wb
 import difflib
 import pandas as pd
+import plotly as plt 
 # Ask the user for a keyword to search for indicators
 search_keyword = str(input('What keyword are you interested in looking for in indicators?: '))
 search_keyword_2 = str(input('What keyword are you interested in looking for in the second indicators?: '))
 
-# List all available indicators
-indicators_list = []
-indicators_list_2 = []
-df = []
-df_2 = []
+# Sets the Global chosen indicators so that they can be called outside the function
 chosen_indicator = ' '
 chosen_indicator_2 = ' '
 
 def indicators(x,y):
+    # Calls the global variables into the function
     global chosen_indicator
     global chosen_indicator_2
+    # sets the counters 
     choice = 1
     choice_2 = 1
+    # Sets the lists for the indicators matching the keywords
+    indicators_list = []
+    indicators_list_2 = []
     # Loop and print indicators matching the keyword
     for x in wb.series.list():
         id_title = x['id']
@@ -31,7 +33,7 @@ def indicators(x,y):
     selection = int(input('\nEnter the number of the indicator you want to use: ')) - 1
     chosen_indicator = indicators_list[selection]['id']
 
-    # Loop and print second indicators matching the keyword
+    # Loop and print indicators matching the second keyword
     for y in wb.series.list():
         id_title_2 = y['id']
         indicator_name_2 = y['value'] 
@@ -44,11 +46,24 @@ def indicators(x,y):
     chosen_indicator_2 = indicators_list_2[selection_2]['id']
 
 
+# Starts the function using the keywords chosen at the top of the code
 indicators(search_keyword,search_keyword_2)
+
+# Modifies and sets the data frames 
 
 df = wb.data.DataFrame(chosen_indicator)
 df_2 = wb.data.DataFrame(chosen_indicator_2)
 
-print(df.head())
-print(df_2.head())
+# I honestly thing that using two different dfs would make it easier to graph and draw comparisons between them
+df_long = df.reset_index().melt(id_vars = 'economy', var_name = 'Year', value_name = chosen_indicator)
+df_2_long = df_2.reset_index().melt(id_vars = 'economy', var_name = 'Year', value_name = chosen_indicator_2)
+df_merge = pd.merge(df_long, df_2_long, on = ['Year'])
+print(df_merge.iloc[:100,:])
 
+# plt.plot(df, usa_pop, label='USA Population', marker='o')
+# plt.plot(years, usa_co2, label='USA CO₂ Emissions', marker='x')
+
+# plt.title('USA Population vs CO₂ Emissions')
+# plt.xlabel('Year')
+# plt.legend()
+# plt.show()
