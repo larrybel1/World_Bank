6ed9397d539d61dc1163d8c977b7f43834c86c3c
Larry Bello-Castillo <belarry2001@gmail.com>
2026-01-17 14:37:35 -0500

    started playing around with the dataset. With in wb_data_4 started writing code that will clean the data up and shorten some of the titles used

--- wb_data_4.py
+++ wb_data_4.py
@@ -4,5 +4,19 @@ import pandas as pd
 import plotly as plt 
 
 df = pd.read_excel("WDI_data.xlsx")
+### Viewing the first 10 rows & Columns to get an idea of what the dataset looks like
+# print(df.iloc[0:10, 0:10])
 
-print(df.head())
+### Making a new df without the following columns, keeping the pervious df becasue it still has important
+column_to_drop = ["Country Code", "Series Code"]
+df_simple = df.drop(columns = column_to_drop)
+
+# df_simple.columns.values[2:] = [c.split("_"[0]) for c in df_simple.columns.values[2:]]
+df_simple.columns.values[2:] = [''.join(filter(str.isdigit, c)) for c in df_simple.columns.values[2:]]
+
+
+
+### Checking new df
+# print(df_simple.iloc[0:10 , 0:10])
+print(df_simple.columns.values[2:])
+
